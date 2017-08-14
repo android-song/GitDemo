1c7e80472442d5371370692652ecfdfa89524794
sun <1105990804@qq.com>
2017-08-14 20:29:17 +0800

    second commit

--- app/src/main/java/com/example/sun/hello/Manager.java
+++ app/src/main/java/com/example/sun/hello/Manager.java
@@ -7,6 +7,7 @@ package com.example.sun.hello;
 public class Manager {
     private static Manager Manager;
     int i=0;
+    int j=0;
     public static Manager getInstance(){
         if (Manager==null){
             Manager=new Manager();
