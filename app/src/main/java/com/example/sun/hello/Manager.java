d46b636848cb24d1560605d1ecaa19d17bbb7e35
sun <1105990804@qq.com>
2017-08-15 16:38:28 +0800

    Manager.java

--- app/src/main/java/com/example/sun/hello/Manager.java
+++ app/src/main/java/com/example/sun/hello/Manager.java
@@ -6,9 +6,7 @@ package com.example.sun.hello;
 
 public class Manager {
     private static Manager Manager;
-        int i=0;
-      String name="";
-    int j=0;
+
     public static Manager getInstance(){
         if (Manager==null){
             Manager=new Manager();
